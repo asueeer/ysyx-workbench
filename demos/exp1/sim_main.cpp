c524d8cec6f554b24b00a2b4cf87fc661e4e8e69
22040114-Yisu Wang <asueeer@163.com>
2022-02-23 16:31:44 +0800

    sim wave by gtkwave

--- demos/exp1/sim_main.cpp
+++ demos/exp1/sim_main.cpp
@@ -3,20 +3,31 @@
 #include <assert.h>
 #include "Vtop.h"
 #include "verilated.h"
+#include "verilated_vcd_c.h"
 
 int main(int argc, char** argv, char** env){
 	VerilatedContext* contextp =  new VerilatedContext;
 	contextp->commandArgs(argc, argv);
 	Vtop* top = new Vtop(contextp);
-	while(!contextp->gotFinish()){
+	
+	Verilated::traceEverOn(true);
+
+	VerilatedVcdC* tfp = new VerilatedVcdC;
+	top->trace(tfp, 99);
+	tfp->open("obj_dir/simx.vcd");
+	int sim_time = 10;
+	while(!contextp->gotFinish() || contextp->time() < sim_time){
+		contextp->timeInc(1);
 		int a = rand() & 1;
 		int b = rand() & 1;
 
 		top->a = a;
 		top->b = b;
 		top->eval();
+		tfp->dump(contextp->time());
 
 		printf("a = %d, b = %d, f = %d\n", a, b, top->f);
 		assert(top->f == a ^ b);
 	}
+	tfp->close();
 }
