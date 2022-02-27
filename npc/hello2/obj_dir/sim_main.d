b2c9b3a6a53a46c3cad76c3a117faed0041e6287
20220114-Yisu Wang <asueeer@163.com>
2022-02-28 00:35:33 +0800

    add nvboard demo, bind pin(.sw[0](a), .sw[1](b), ledr[0](f))

--- npc/hello2/obj_dir/sim_main.d
+++
@@ -1,5 +0,0 @@
-sim_main.o: ../csrc/sim_main.cpp Vour.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h
