c0e5c1145ccf7cb9f0a63b810bda7dca996786d6
asu <asueeer@163.com>
2022-03-02 23:58:46 +0800

    尝试测试无符号减法

--- npc/exps/ALU/main.cpp
+++ npc/exps/ALU/main.cpp
@@ -54,8 +54,8 @@ int main() {
 
     // 测试有符号的加法, 有符号加法只关心溢出 不关心进位/借位
     top->sel = 0b000;
-    for (int i = -8; i < 7; ++i) {
-        for (int j = -8; j < 7; ++j) {
+    for (int i = -8; i <= 7; ++i) {
+        for (int j = -8; j <= 7; ++j) {
             int sum = i + j;
             int of = over_flow(sum);
             sum &=0b1111;
@@ -73,8 +73,8 @@ int main() {
 
     // 测试无符号的加法, 无符号的加法只关心进位, 不关心溢出
     top->sel = 0b000;
-    for (unsigned int i = 0; i < 15; ++i) {
-        for (unsigned int j = 0; j < 15; ++j) {
+    for (unsigned int i = 0; i <= 15; ++i) {
+        for (unsigned int j = 0; j <= 15; ++j) {
             int sum = i + j;
             int carry = carry_(sum);
             sum &=0b1111;
@@ -82,7 +82,7 @@ int main() {
             top->B = j;
             step_and_dump_wave();
             if (top->result != sum || top->carry != carry) {
-                printf("i: %d, j: %d, your_sum: %d, your_carry: %d\n", i, j, top->result, top->overflow);
+                printf("i: %d, j: %d, your_sum: %d, your_carry: %d\n", i, j, top->result, top->carry);
                 printf("sum should be: %d, carry should be %d\n", sum, carry);
                 sim_exit();
                 exit(0);
@@ -92,8 +92,8 @@ int main() {
 
     // 测试有符号的减法, 有符号的减法只关心溢出, 不关心进位/借位
     top->sel = 0b001;
-    for (int i = -8; i < 7; ++i) {
-        for (int j = -8; j < 7; ++j) {
+    for (int i = -8; i <= 7; ++i) {
+        for (int j = -8; j <= 7; ++j) {
             int sum = i - j;
             int of = over_flow(sum);
             sum &=0b1111;
@@ -102,6 +102,25 @@ int main() {
             step_and_dump_wave();
             if (top->result != sum || top->overflow != of) {
                 printf("i: %d, j: %d, your_sum: %d, your_of: %d\n", i, j, top->result, top->overflow);
+                printf("sum should be: %d, of should be %d\n", sum, of);
+                sim_exit();
+                exit(0);
+            }
+        }
+    }
+
+    // 测试无符号的减法, 无符号的减法只关心借位, 不关心溢出
+    top->sel = 0b001;
+    for (unsigned int i = 0; i <= 15; ++i) {
+        for (unsigned int j = 0; j <= 15; ++j) {
+            int sum = i - j;
+            int borrow = borrow_(sum);
+            sum &=0b1111;
+            top->A = i;
+            top->B = j;
+            step_and_dump_wave();
+            if (top->result != sum || top->carry != !borrow) {
+                printf("i: %d, j: %d, your_sum: %d, your_carry: %d\n", i, j, top->result, top->carry);
                 printf("sum should be: %d, of should be %d\n", sum, of);
                 sim_exit();
                 exit(0);
