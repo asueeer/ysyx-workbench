09991661df1b799748be6b7d1b49c53517c36d5c
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-07 20:50:25 +0800

    >  compile NEMU ysyx_20220114 Yisu Wang Linux asu-diy 5.13.0-30-generic #33~20.04.1-Ubuntu SMP Mon Feb 7 14:25:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:50:25 up  4:28,  1 user,  load average: 0.31, 0.40, 0.34

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -67,7 +67,7 @@ static int cmd_x(char *args) {
     int N = atoi(ptr);
     ptr = strtok(NULL, delim);
     word_t addr = strtol(ptr, NULL, 16); // fixme: ptr might be an expr
-    printf("N is %d, addr is 0x%lx, val is 0x%lx\n", N, addr, paddr_read(addr, 100));
+    printf("N is %d, addr is 0x%lx, val is 0x%lx\n", N, addr, paddr_read(addr, 4));
     return 0;
 }
 
