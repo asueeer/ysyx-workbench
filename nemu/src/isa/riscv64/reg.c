ab14509487d13ff5ec59a4cfb6892c0bbab8e6a6
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-07 19:28:48 +0800

    >  compile NEMU ysyx_20220114 Yisu Wang Linux asu-diy 5.13.0-30-generic #33~20.04.1-Ubuntu SMP Mon Feb 7 14:25:10 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:28:48 up  3:07,  1 user,  load average: 0.30, 0.28, 0.18

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -1,22 +1,30 @@
 #include <isa.h>
+#include <stdio.h>
 #include "local-include/reg.h"
 
 const char *regs[] = {
-  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+        "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+        "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+        "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+        "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
 word_t isa_reg_str2val(const char *s, bool *success);
 
 void isa_reg_display() {
-    cpu.gpr[1] = 0x100;
-    for (int i = 0; i < 32; i++){
-        printf("%s\t0x%lx\t\t%ld\n", regs[i], gpr(i), gpr(i));
+    for (int i = 0; i < 32; ++i) {
+        bool success;
+        word_t reg_val = isa_reg_str2val(regs[i], &success);
+        printf("%s\t0x%lx\t\t%ld\n", regs[i], reg_val, reg_val);
     }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
-  return 0;
+    for (int i = 0; i < 32; ++i) {
+        if (strcmp(s, regs[i]) == 0) {
+            return gpr(i);
+        }
+    }
+    *success = 0;
+    return 0;
 }
