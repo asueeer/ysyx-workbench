8b48b85e6e70c5224befdb4620611f7643d75bcc
20220114-Yisu Wang <asueeer@163.com>
2022-03-21 13:46:58 +0800

    change match rules order, cacl the expr value

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -25,17 +25,15 @@ static struct rule {
          * Pay attention to the precedence level of different rules.
          */
 
-        {" +",      TK_NOTYPE},   // spaces
+        {" +",       TK_NOTYPE},   // spaces
         {"-?[0-9]+", TK_INT},      // Integer
-        {"\\+",     '+'},         // plus
-        {"\\-",     '-'},         // minus
-        {"\\*",     '*'},         // multiply
-        {"\\/",     '/'},         // divide
-        {"\\(",     '('},
-        {"\\)",     ')'},
-
-        //{"[0-9]+",  TK_INT},      // Integer
-        {"==",      TK_EQ},       // equal
+        {"\\+",      '+'},         // plus
+        {"\\-",      '-'},         // minus
+        {"\\*",      '*'},         // multiply
+        {"\\/",      '/'},         // divide
+        {"\\(",      '('},
+        {"\\)",      ')'},
+        {"==",       TK_EQ},       // equal
 };
 
 #define NR_REGEX ARRLEN(rules)
