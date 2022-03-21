68c0eb597bc58ac27a9f053f5846ee374de27739
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-21 11:58:15 +0800

    >  compile NEMU ysyx_20220114 Yisu Wang Linux asu-diy 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:58:15 up 13:18,  1 user,  load average: 0.40, 0.28, 0.26

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -29,7 +29,7 @@ static struct rule {
         {"\\/",         '/'},         // divide
         {"\\(",         '('},
         {"\\)",         ')'},
-        {"^[1-9]\\d+$", TK_INT},      // Integer
+        {"^[0-9]\\d+$", TK_INT},      // Integer
         {"==",          TK_EQ},       // equal
 };
 
