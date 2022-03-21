3eec8ad52472840f86f1c9fe071d33c588d8acfc
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-21 11:25:55 +0800

    >  compile NEMU ysyx_20220114 Yisu Wang Linux asu-diy 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:25:55 up 12:46,  1 user,  load average: 0.74, 0.66, 0.48

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -93,7 +93,7 @@ static bool make_token(char *e) {
                 }
                 switch (rules[i].token_type) {
                     case TK_INT:
-                        strncpy(tokens[nr_token].str, substr_start, substr_len);
+                        // strncpy(tokens[nr_token].str, substr_start, substr_len);
                         tokens[nr_token].str[substr_len] = '\0';
                         tokens[nr_token].type = rules[i].token_type;
                         break;
