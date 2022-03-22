4edfd6f1932ee56dd2d1c6eab6d5c75069bfd759
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-22 19:16:26 +0800

    >  compile NEMU ysyx_20220114 Yisu Wang Linux asu-diy 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:16:26 up 1 day, 20:36,  1 user,  load average: 0.39, 0.64, 0.61

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -68,6 +68,10 @@ static int cmd_x(char *args) {
     char *ptr = strtok(args, delim);
     int N = atoi(ptr);
     ptr = strtok(NULL, delim);
+    if (!ptr) {
+        printf("cmd_x: strtok fail.\n");
+        return 0;
+    }
     word_t addr = strtol(ptr, NULL, 16); // fixme: ptr might be an expr
     for (int i = 0; i < N; ++i) {
         printf("\x1b[94m0x%lx\x1b[0m: 0x%lx \n", addr, paddr_read(addr, 4));
