5432495ab8f7ee9f615a0a63555ff62c8280c4e9
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-28 13:32:05 +0800

    >  compile NEMU ysyx_20220114 Yisu Wang Linux asu-diy 5.13.0-44-generic #49~20.04.1-Ubuntu SMP Wed May 18 18:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:32:05 up  2:18,  1 user,  load average: 0.24, 0.49, 0.65

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -141,7 +141,7 @@ void cpu_exec(uint64_t n) {
 int update_ringbuf(Decode *s, char *str) {
     s->ring_idx = (s->ring_idx + 1) % 32;
     if (s->ringbuf[s->ring_idx] == NULL) {
-        memset(s->ringbuf[s->ring_idx], '\0', 128);
+        memset(s->ringbuf[s->ring_idx], 0, 128);
     }
     strcpy(s->ringbuf[s->ring_idx], str);
     return 0;
